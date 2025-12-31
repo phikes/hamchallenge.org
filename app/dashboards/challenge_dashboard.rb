eea16a4e37c6da4534549f37f205297db3820aba
Phillip Kessels <info@phikes.de>
2025-12-31 14:54:07 +0100

    feat: show week in challenge dashboard

--- app/dashboards/challenge_dashboard.rb
+++ app/dashboards/challenge_dashboard.rb
@@ -26,7 +26,7 @@ class ChallengeDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    id
+    week
     summary
     category
     difficulty
